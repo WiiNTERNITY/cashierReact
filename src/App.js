7087bab4ad292afb4cfec844aff178213a71dbb7
wiinternity <hammani_mounir@outlook.fr>
2023-01-24 16:15:35 +0100

    commit cashier

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Caisse from "./components/Caisse"
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Caisse />
+      </div>
+    );
+  }
+}
+
+export default App;
